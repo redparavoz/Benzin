a2f6956f50349126ac4edd64ef51f29f788a4b70
redparavoz <shanghaioftrain@gmail.com>
2019-02-21 19:06:10 +0300

    benzin

---
+++ BenZin.py
@@ -0,0 +1,13 @@
+
+print("Gasoline calculation")
+print()
+print("Please enter the mileage of day")
+prais_for_liter = eval(input("prais for liters:"))
+gas_mileage = eval(input("gas mileage:"))
+mileage = eval(input("mileage:"))
+total = prais_for_liter * gas_mileage * mileage /100  #IDLE do calculation
+print()
+print ("The total value of your mileage is", total)
+    
+
+
